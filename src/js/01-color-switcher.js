6b355063394baf98231873d174d725a047619781
revut <revutskyg@gmail.com>
2021-11-16 21:49:38 +0200

    color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+// const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
 
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+
+const hendlStart = () => {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.btnStart.setAttribute('disabled', true);
+};
+
+const hendlStop = () => {
+  clearInterval(timerId);
+  refs.btnStart.removeAttribute('disabled');
+};
+
+refs.btnStart.addEventListener('click', hendlStart);
+refs.btnStop.addEventListener('click', hendlStop);
