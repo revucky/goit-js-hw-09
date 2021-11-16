928ced7429d11f886c91e1be42b41f34ee400f41
revut <revutskyg@gmail.com>
2021-11-16 21:54:00 +0200

    pack

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,3 @@
-// const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
-
 const refs = {
   body: document.querySelector('body'),
   btnStart: document.querySelector('button[data-start]'),
